21e04c4668144f787903782c0d8cc3a9fac66ab2
stevenbai0724 <steven.bai@student.tdsb.on.ca>
2021-10-01 23:39:53 -0400

    10/1/21

---
+++ server.js
@@ -0,0 +1,31 @@
+const express = require('express')
+const bodyParser = require('body-parser');
+const app = express()
+const axios = require('axios')
+
+app.use(express.json())
+
+app.get('/api/user', async (req, res) => {
+
+    const API_KEY = "";
+
+
+    const requestOptions = {
+        headers: { 
+            Authorization: `Bearer ${API_KEY}`
+        },
+    }
+    const res2 = await axios.get('https://dmoj.ca/api/v2/users', requestOptions)
+
+
+
+    var profile = (res2.data.data);
+
+    res.json(profile)
+
+
+})
+
+const port = 5000;
+
+app.listen(port, () => console.log(`Server started on port ${port}`));
