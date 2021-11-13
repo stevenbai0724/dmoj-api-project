0086f80f9647006a99d03eacac5560e9dfbcd4e1
Steven <stevenbai0724@gmail.com>
2021-11-13 15:17:34 -0500

    deploy

--- server.js
+++ server.js
@@ -9,9 +9,11 @@ const PORT = process.env.PORT || 5000;
 app.use(express.static(path.resolve(__dirname, '../client/build')));
 app.use(express.json())
 app.get('/api', (req, res) => {    res.json({message : "Hello from express server"})});
-app.get('*', (req, res) => {
-    res.sendFile(path.resolve(__dirname, '../client/build', 'index.html'));
-  });
+app.get('*', function (req, res) {
+    const index = path.join(__dirname, 'build', 'index.html');
+    res.sendFile(index);
+
+});
 app.post('/api/user', async (req, res) => {
 
     const API_KEY = "AADIywXVen_ihhJ1KmIEyQMF4O0atL5Nh6zrNseUi03lXl0T";
